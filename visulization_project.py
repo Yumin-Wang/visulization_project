f6c5e3154fb06532a2c2e07075d8e02a5ec03c4f
yifanwang0715 <yifanwang0715@outlook.com>
2022-04-22 17:19:33 -0400

    year

--- visulization_project.py
+++ visulization_project.py
@@ -36,8 +36,7 @@ source = alt.topo_feature('https://cdn.jsdelivr.net/npm/vega-datasets@v1.29.0/da
 st.write("## COVID-19 Worldwide Metrics Over Time")
 
 
-#year=st.sidebar.slider(label='Year', min_value=min(df['year']), max_value=max(df['year']), step=1, value=min(df['year']))
-year = st.sidebar.multiselect(label='Year', options=list(df['year'].unique()))
+year=st.sidebar.slider(label='Year', min_value=min(df['year']), max_value=max(df['year']), step=1, value=min(df['year']))
 subset = df[df["year"] == year]
 
 
