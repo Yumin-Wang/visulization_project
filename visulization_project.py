954e1c5e7a69aa82728232a37eebb49bf2ec7672
carocann <89943872+carocann@users.noreply.github.com>
2022-05-02 12:05:10 -0400

    added more data from the dataset to be able to load

--- visulization_project.py
+++ visulization_project.py
@@ -11,7 +11,8 @@ import datetime
 def load_data():
     covid = pd.read_csv("https://raw.githubusercontent.com/Yumin-Wang/visulization_project/main/owid-covid-data.csv")
     country_df = pd.read_csv('https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/country_codes.csv', dtype = {'conuntry-code': str})[['Country','country-code']]
-    covid = covid[['iso_code','continent','location','date','total_cases_per_million','new_cases_per_million','total_deaths_per_million','reproduction_rate','population']]
+    covid = covid[['iso_code','continent','location','date','total_cases_per_million','new_cases_per_million','total_deaths_per_million','reproduction_rate','population',
+    'median_age','handwashing_facilities','hospital_beds_per_thousand','life_expectancy','diabetes_prevalence','female_smokers','male_smokers']]
     covid['date'] = pd.to_datetime(covid['date'])
     covid = covid[(covid['date']>='2020-03-01')&(covid['date']<='2022-03-31')]
     covid['month'] = covid['date'].dt.strftime('%B')
@@ -22,6 +23,16 @@ def load_data():
     covid['total_deaths_per_million']= covid['total_deaths_per_million'].fillna(method='bfill').fillna(method='ffill')
     covid['reproduction_rate']= covid['reproduction_rate'].fillna(method='bfill').fillna(method='ffill')
     country_df['Country'] = country_df['Country'].replace(['United States of America','United Kingdom of Great Britain and Northern Ireland'],['United States','United Kingdom'])
+    covid['median_age'] = covid['median_age'].fillna(method='bfill').fillna(method='ffill')
+    covid['handwashing_facilities'] = covid['handwashing_facilities'].fillna(method='bfill').fillna(method='ffill')
+    covid['hospital_beds_per_thousand'] = covid['hospital_beds_per_thousand'].fillna(method='bfill').fillna(method='ffill')
+    covid['life_expectancy'] = covid['life_expectancy'].fillna(method='bfill').fillna(method='ffill')
+    covid['diabetes_prevalance'] = covid['diabetes_prevalance'].fillna(method='bfill').fillna(method='ffill')
+    covid['female_smokers'] = covid['female_smokers'].fillna(method='bfill').fillna(method='ffill')
+    covid['male_smokers'] = covid['male_smokers'].fillna(method='bfill').fillna(method='ffill')
+
+
+
     covid.rename(columns = {'location' : 'Country'}, inplace = True)
     covid = covid.merge(country_df,how='left',on='Country')
     covid.dropna(inplace=True)
