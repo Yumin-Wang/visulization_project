6bb9a41eeaef9ec75678a5638f840a37809e5f15
yifanwang0715 <yifanwang0715@outlook.com>
2022-05-02 18:12:34 -0400

    uploading owid data

--- visulization_project.py
+++ visulization_project.py
@@ -76,103 +76,103 @@ bar_data = bar_data.groupby(['Country', 'country-code']).mean().reset_index()
 metric = st.sidebar.radio(label='Metrics', options=['total_cases_per_million','new_cases_per_million','total_deaths_per_million'], index=1)
 metric_title = metric.replace('_', ' ')
 
-# #World_map
-# width_worldmap=600
-# height_worldmap=300
+#World_map
+width_worldmap=600
+height_worldmap=300
 
-# selector = alt.selection_single(
-#     on="click")
+selector = alt.selection_single(
+    on="click")
 
-# background = alt.Chart(source
-# ).mark_geoshape(
-#     fill='#aaa',
-#     stroke='white'
-# ).properties(
-#     width=width_worldmap,
-#     height=height_worldmap
-# ).project('equirectangular')
+background = alt.Chart(source
+).mark_geoshape(
+    fill='#aaa',
+    stroke='white'
+).properties(
+    width=width_worldmap,
+    height=height_worldmap
+).project('equirectangular')
 
-# worldmap_base =alt.Chart(source
-#     ).properties( 
-#         width=width_worldmap,
-#         height=height_worldmap
-#     ).project('equirectangular'
-#     ).add_selection(
-#         selector
-#     ).transform_lookup(
-#         lookup="id",
-#         from_=alt.LookupData(covid_map_data, "country-code", ["Country",metric, 'population']),
-#     ).transform_filter(
-#         selector
-#     )
+worldmap_base =alt.Chart(source
+    ).properties( 
+        width=width_worldmap,
+        height=height_worldmap
+    ).project('equirectangular'
+    ).add_selection(
+        selector
+    ).transform_lookup(
+        lookup="id",
+        from_=alt.LookupData(covid_map_data, "country-code", ["Country",metric, 'population']),
+    ).transform_filter(
+        selector
+    )
 
-# rate_scale = alt.Scale(domain=[covid_map_data[metric].min(), covid_map_data[metric].max()])
-# rate_color = alt.Color(field=metric, type="quantitative", scale=rate_scale)
-# chart_worldmap = background+worldmap_base.mark_geoshape(stroke="black", strokeWidth=0.15).encode(
-#     color=rate_color,
-#         tooltip=[
-#             alt.Tooltip(field=metric,type='quantitative', title=f"{metric_title} averaged over month"),
-#             alt.Tooltip("Country:N", title="Country"),
-#         ]
-#     ).properties(
-#     title=f'World map for {metric_title} averaged in {month} of {year}'
-# )
+rate_scale = alt.Scale(domain=[covid_map_data[metric].min(), covid_map_data[metric].max()])
+rate_color = alt.Color(field=metric, type="quantitative", scale=rate_scale)
+chart_worldmap = background+worldmap_base.mark_geoshape(stroke="black", strokeWidth=0.15).encode(
+    color=rate_color,
+        tooltip=[
+            alt.Tooltip(field=metric,type='quantitative', title=f"{metric_title} averaged over month"),
+            alt.Tooltip("Country:N", title="Country"),
+        ]
+    ).properties(
+    title=f'World map for {metric_title} averaged in {month} of {year}'
+)
 
-# #Trend line for metric
-# metric_base = alt.Chart(subset
-#  ).mark_line().encode(
-#     x=alt.X('date:O', title='Date'),
-#     y=alt.Y(field=metric,type='quantitative', title=metric.replace('_', ' ').title()),
-#     color='Country:N'
-# ).properties(
-#     width=400,
-#     height=300
-# ) 
+#Trend line for metric
+metric_base = alt.Chart(subset
+ ).mark_line().encode(
+    x=alt.X('date:O', title='Date'),
+    y=alt.Y(field=metric,type='quantitative', title=metric.replace('_', ' ').title()),
+    color='Country:N'
+).properties(
+    width=400,
+    height=300
+) 
 
-# brush_metric =  alt.selection(type='interval', encodings=['x'])
+brush_metric =  alt.selection(type='interval', encodings=['x'])
 
-# metric_chart_detail = metric_base.transform_filter(brush_metric).properties(title=f"Compare {metric_title} in selected countries in during {month} of {year}")
-# metric_chart_global = metric_base.properties(height=60).add_selection(brush_metric)
-
-
-# #Trend line for reproduction rate
-# r_base = alt.Chart(subset
-#  ).mark_line().encode(
-#     x=alt.X('date:O', title='Date'),
-#     y=alt.Y(field="reproduction_rate",type='quantitative', title='Reproduction Rate'),
-#     color='Country:N'
-# ).properties(
-#     width=400,
-#     height=300
-# ) 
-
-# brush_r =  alt.selection(type='interval', encodings=['x'])
-# r_chart_detail = r_base.transform_filter(brush_r).properties(title=f"Compare reproduction rate in selected countries in during {month} of {year}")
-# r_chart_global = r_base.properties(height=60).add_selection(brush_r)
+metric_chart_detail = metric_base.transform_filter(brush_metric).properties(title=f"Compare {metric_title} in selected countries in during {month} of {year}")
+metric_chart_global = metric_base.properties(height=60).add_selection(brush_metric)
 
 
-# #Bar chart
-# bar = alt.Chart(bar_data).mark_bar().encode(
-#     y=alt.Y(field=metric, type="quantitative"),
-#     x=alt.X(field="Country", type="nominal"),
-#     color='Country:N',
-#     tooltip=[
-#             alt.Tooltip(field=metric, type="quantitative", title=f"{metric_title} average over month"),
-#             alt.Tooltip("Country:N", title="Country")]
-#             ).properties(width=250,title=f'Compare {metric_title} averaged in {month} of {year} for selected countries')
+#Trend line for reproduction rate
+r_base = alt.Chart(subset
+ ).mark_line().encode(
+    x=alt.X('date:O', title='Date'),
+    y=alt.Y(field="reproduction_rate",type='quantitative', title='Reproduction Rate'),
+    color='Country:N'
+).properties(
+    width=400,
+    height=300
+) 
+
+brush_r =  alt.selection(type='interval', encodings=['x'])
+r_chart_detail = r_base.transform_filter(brush_r).properties(title=f"Compare reproduction rate in selected countries in during {month} of {year}")
+r_chart_global = r_base.properties(height=60).add_selection(brush_r)
 
 
-# chart_trend=alt.hconcat(metric_chart_detail&metric_chart_global, r_chart_detail&r_chart_global).resolve_scale(color='independent')
+#Bar chart
+bar = alt.Chart(bar_data).mark_bar().encode(
+    y=alt.Y(field=metric, type="quantitative"),
+    x=alt.X(field="Country", type="nominal"),
+    color='Country:N',
+    tooltip=[
+            alt.Tooltip(field=metric, type="quantitative", title=f"{metric_title} average over month"),
+            alt.Tooltip("Country:N", title="Country")]
+            ).properties(width=250,title=f'Compare {metric_title} averaged in {month} of {year} for selected countries')
 
-# chart_trend_worldmap=alt.vconcat(chart_trend, chart_worldmap).resolve_scale(color='independent')
 
-# chart_final = alt.vconcat(chart_trend_worldmap, bar).resolve_scale(color='independent')
+chart_trend=alt.hconcat(metric_chart_detail&metric_chart_global, r_chart_detail&r_chart_global).resolve_scale(color='independent')
 
-# st.altair_chart(chart_final, use_container_width=True)
+chart_trend_worldmap=alt.vconcat(chart_trend, chart_worldmap).resolve_scale(color='independent')
 
-# #st.altair_chart(metric_chart_detail&metric_chart_global, use_container_width=True)
-# #st.altair_chart(chart_worldmap, use_container_width=True)
-# #st.altair_chart(donut, use_container_width=True)
+chart_final = alt.vconcat(chart_trend_worldmap, bar).resolve_scale(color='independent')
+
+st.altair_chart(chart_final, use_container_width=True)
+
+#st.altair_chart(metric_chart_detail&metric_chart_global, use_container_width=True)
+#st.altair_chart(chart_worldmap, use_container_width=True)
+#st.altair_chart(donut, use_container_width=True)
 
 
 
