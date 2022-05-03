93d84a52f56079c27ef42deb10fc4e6d083ce4c8
yifanwang0715 <yifanwang0715@outlook.com>
2022-05-02 22:25:41 -0400

    total vaccinations

--- visulization_project.py
+++ visulization_project.py
@@ -173,7 +173,7 @@ st.altair_chart(chart_final, use_container_width=True)
 
 #vaccination bar chart
 vaccine_bar = alt.Chart(bar_data).mark_bar().encode(
-    y=alt.Y(field='sum(total_vaccinations)', type="quantitative"),
+    y=alt.Y(field='total_vaccinations', type="quantitative"),
     x=alt.X(field="Country", type="nominal"),
     color='Country:N',
     tooltip=[
