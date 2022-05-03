47746c0915dae33bfe973d3c54d448678fd73316
carocann <89943872+carocann@users.noreply.github.com>
2022-05-03 11:46:48 -0400

    max vax

--- visulization_project.py
+++ visulization_project.py
@@ -165,7 +165,7 @@ bar = alt.Chart(bar_data).mark_bar().encode(
 
 #vaccination bar chart
 vaccine_bar = alt.Chart(bar_data).mark_bar().transform_aggregate(
-    most_recent_vax='argmax(vaccination)',
+    most_recent_vax='argmax(total_vaccination)',
     groupby=['Country']
 ).transform_calculate(
     correct_vax='datum.most_recent_vax.vaccination'
